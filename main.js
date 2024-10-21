59c20d94651809d7c5e498630487fa99c65aa7ef
Yomirose <roselinefayomi10@yahoo.comcleargit config --global user.email roselinefayomi10@yahoo.com>
2024-10-21 21:09:15 +0100

    Work in progress, fetch, pet data

---
+++ main.js
@@ -0,0 +1,21 @@
+async function start() {
+    const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+    const weatherData = await weatherPromise.json()
+    // console.log(weatherData)
+
+    const ourTemperature = weatherData.properties.periods[0].temperature
+    document.querySelector("#temperature-output").textContent = ourTemperature
+    // console.log(ourTemperature)
+}
+
+start()
+
+async function petsArea() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    petsData.forEach( pet => {
+       console.log(pet.species) 
+    });
+}
+
+petsArea()
