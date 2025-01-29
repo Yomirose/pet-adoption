e631173d77ed7388293878512c0a130063361189
Yomirose <roselinefayomi10@yahoo.comcleargit config --global user.email roselinefayomi10@yahoo.com>
2025-01-29 10:09:12 +0100

    update pets area section

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
     const weatherData = await weatherPromise.json()
-    // console.log(weatherData)
 
     const ourTemperature = weatherData.properties.periods[0].temperature
     document.querySelector("#temperature-output").textContent = ourTemperature
-    // console.log(ourTemperature)
 }
 
 start()
@@ -13,8 +11,9 @@ start()
 async function petsArea() {
     const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
     const petsData = await petsPromise.json()
+    console.log(petsData)
     petsData.forEach( pet => {
-       console.log(pet.species) 
+        const clone = template.content.cloneNode(true)
     });
 }
 
